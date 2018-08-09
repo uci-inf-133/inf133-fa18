436bf9cfc382a6213efeef8f90b990aa5662929d
Daniel Epstein <epstein@ics.uci.edu>
2018-08-09 16:39:15 -0700

    Generate .ics

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const ics = require('ics');
+const fs = require('fs-extra');
+var moment = require('moment');
+
+/* Create .ics calendar file */
+var calendar_data = fs.readJsonSync('public/calendar.json');
+var calendar_events = [].concat.apply([], calendar_data.events.map((e) => {
+	return calendar_data[e.type].map((l) => {
+		var time = moment(e.date + " " + l.time);
+		return {
+			"title": l.name + ": " + e.title,
+			"start": [time.year(), time.month() + 1, time.date(), time.hour(), time.minute()],
+			"duration": {minutes: l.duration},
+			"location": l.location
+		};
+	});
+}));
+
+ics.createEvents(calendar_events, (error, value) => {
+	if(error) {
+		console.log(error);
+	} else {
+		fs.writeFileSync('public/calendar.ics', value);
+	}
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
